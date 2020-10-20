c64ca24332fc635287bce3428dcf2b3a44c0cfae
Fernando C <fernandoeqc@live.com>
2020-10-19 23:00:33 -0300

    exe 6
    refatorar para otimizar o tempo

--- 10001st prime.py
+++ 10001st prime.py
@@ -1,13 +1,35 @@
 """ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-What is the 10 001st prime number? """
+What is the 10001st prime number? """
 
-dividendo = 1
-divisor = 1
-cont_resultado = 1
+import time
 
-while cont_resultado != 6:
-    for div in range(dividendo):
-        if dividendo % div == 0:
-            cont_resultado += 1
-            continue
+t = time.time()
+
+
+def primo(numero):
+    dividendo = numero
+    
+    for div in range(2, numero, 1):
+        if (dividendo % div) == 0:
+            return False
+
+    return True
+
+
+lista = []
+numeracao = 2
+contagem = 0
+
+
+while contagem != 10001:
+    if (primo(numeracao)) is True:
+        lista.append(numeracao)
+        print(numeracao, contagem)
+        contagem += 1
+    numeracao += 1
+
+print("fim")
+
+print(time.time() - t)
+""" 82.43018507957458 s """
